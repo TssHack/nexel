b38a3613ab03b80e1972f7719f735393811e2df3
Ehsan <144959137+TssHack@users.noreply.github.com>
2025-09-16 15:58:13 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+API_ID = 18377832
+API_HASH = "ed8556c450c6d0fd68912423325dd09c"
+SESSION_NAME = "my_ai_multilang"
+DB_FILE = "users_data.db"
+
+# API Endpoints
+GPT4_API_URL = "https://api.binjie.fun/api/generateStream"
+LAMA_API_URL = "http://lama-ehsan.vercel.app/chat"
+GEMINI_API_URL = "https://gem-ehsan.vercel.app/gemini/chat"
+
+# Default Settings
+DEFAULT_AI_MODEL = "gpt4"
+DEFAULT_LANGUAGE = "fa"
